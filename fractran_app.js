78625428f216c0f4a8866c41bf0549fbb842a6aa
tjwei <tzerjen@gmail.com>
2017-09-10 21:07:56 +0800

    speed up

--- fractran_app.js
+++ fractran_app.js
@@ -2,24 +2,29 @@
 var $ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
 var prime_table = [2];
+
 function construct_prime_table() {
-    for (var i = 3; i < 1000; i += 2)
+    for (var i = 3; i < 300; i += 2)
         if (bigInt(i).isPrime()) {
             prime_table.push(i);
         }
 }
 construct_prime_table();
+
 function fac_math(fac) {
     return Object.entries(fac).map(a =>
         String(a[0]) + (a[1] == 1 ? "" : `^{${a[1]}}`)
     ).join(' \\cdot ')
 }
+
 function semi_factorization(n0, primes = prime_table) {
     var n = bigInt(n0);
     var i = 0;
     var rtn = {};
     if (n.eq(1)) {
-        return { 1: 1 }
+        return {
+            1: 1
+        }
     }
     while (i < primes.length) {
         var r = n.divmod(primes[i]);
@@ -28,8 +33,7 @@ function semi_factorization(n0, primes = prime_table) {
             n = r.quotient;
             if (n.eq(1))
                 break;
-        }
-        else {
+        } else {
             i += 1;
         }
     }
@@ -90,14 +94,14 @@ var Fractran = {
         if (q.compare(1) == 0) {
             pointer = 0;
             n = p;
-        }
-        else {
+        } else {
             pointer = pointer + 1;
         }
         return [pointer, n, p, q]
 
     }
 }
+
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -106,8 +110,8 @@ var app = new Vue({
     el: '#fractran',
 
     methods: {
-        output_base_changed(text){
-            this.output_base = text.match(/\d+/g).map(s=>parseInt(s));
+        output_base_changed(text) {
+            this.output_base = text.match(/\d+/g).map(s => parseInt(s));
         },
         input_changed(text) {
             this.code_errors = Fractran.check_input_error(text);
@@ -117,9 +121,10 @@ var app = new Vue({
             return true;
         },
         async run() {
+            this.output_base_changed($('#output_base_input').value)
             if (!this.input_changed($('#input_input').value)) {
                 return;
-            } 
+            }
             if (this.code_mode == 0)
                 if (!this.toggle_code_mode())
                     return;
@@ -129,34 +134,51 @@ var app = new Vue({
             code = code.map(s => s.split('/').map(n => bigInt(n)));
             console.log(code)
             this.pointer = 0;
-            var pointer = 0, n2, p, q
+            var pointer = 0,
+                n2, p, q
+            var skip_count = 0;
+            var skip = 0;
             while (this.pointer < code.length) {
                 var f = code[pointer];
                 [pointer, n2, p, q] = Fractran.step(pointer, n, code);
-                var fac_n = semi_factorization(n)
-                var fac_p = semi_factorization(p)
-                var fac_q = semi_factorization(q)
+                var fac_p
                 //console.log(pointer, n2.toString(), p.toString(), q.toString())
                 this.current_ok = q.compare(1) == 0;
                 var in_text = this.current_ok ? "\\in" : "\\not\\in"
-                var text1 = `${fac_math(fac_n)} `
-                var text2 = `\\times \\frac{${f[0]}}{${f[1]}} = `
-                var text3 = `\\frac{${fac_math(fac_p)}}{${fac_math(fac_q)}} `
-                var text4 = `${in_text} \\mathbb{N}`
-                this.step_text = text1 + text2 + text3 + text4
                 var speedup = this.speedup;
-                if(speedup < 0.01) speedup=1;
-                if(this.output_base.length>0 && this.current_ok){
+                if (speedup < 0.01) speedup = 1;
+                if (speedup >= 20000) {
+                    skip = speedup / 10000;
+                } else skip = 0;
+                if (this.output_base.length > 0 && this.current_ok) {
+                    fac_p = semi_factorization(p)
                     console.log(fac_math(fac_p), this.output_base, Object.keys(fac_p))
-                    if(Object.keys(fac_p).map(s=>parseInt(s)).every(k=>{
-                        console.log(k,this.output_base.indexOf(k))
-                    return this.output_base.indexOf(k)>-1})
-                    ){
-                        console.log("add")
+                    if (Object.keys(fac_p).map(s => parseInt(s)).every(k => {
+                            console.log(k, this.output_base.indexOf(k))
+                            return this.output_base.indexOf(k) > -1
+                        })) {
                         this.output_seq.push(`${fac_math(fac_p)}`)
                     }
                 }
-                await timeout((this.current_ok ? 1000 : 200)/speedup);
+                if (speedup < 10000 || this.current_ok) {
+                    if (skip > 0 && skip_count < skip) {
+                        skip_count += 1;
+                    } else {
+                        fac_p = fac_p || semi_factorization(p)
+                        var fac_n = semi_factorization(n)
+                        var fac_q = semi_factorization(q)
+                        var text1 = `${fac_math(fac_n)} `
+                        var text2 = `\\times \\frac{${f[0]}}{${f[1]}} = `
+                        var text3 = `\\frac{${fac_math(fac_p)}}{${fac_math(fac_q)}} `
+                        var text4 = `${in_text} \\mathbb{N}`
+                        this.step_text = text1 + text2 + text3 + text4
+                        await timeout((this.current_ok ? 1000 : 200) / speedup);
+                        skip_count = 0;
+                    }
+                }
+
+
+
                 n = n2;
                 this.pointer = pointer
             }
@@ -189,7 +211,7 @@ var app = new Vue({
         code_modes: ["edit", "view"],
         code_mode: 0,
         //code_text: "455/33 11/13 1/11 3/7 11/2 1/3",
-        code_text: "455/33 11/13 1/11 3/7 11/2 1/3",
+        code_text: "17/91, 78/85, 19/51, 23/38, 29/33, 77/29, 95/23, 77/19, 1/17, 11/13, 13/11, 15/14, 15/2, 55/1",
         code_errors: [],
         current_ok: true,
         input_number: "8",
@@ -202,12 +224,12 @@ var app = new Vue({
         examples: []
     },
     computed: {
-        output_seq_math(){
+        output_seq_math() {
             var s = this.output_seq.join(" , ");
             return katex.renderToString(s);
         },
-        output_base_math(){
-            var s = this.output_base.map(n=>`${n}^{?}`).join(" ");
+        output_base_math() {
+            var s = this.output_base.map(n => `${n}^{?}`).join(" ");
             return katex.renderToString(s);
         },
         input_math() {
